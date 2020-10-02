a2d04a4674d06e5e58e24ba7062c408a43693525
Shahlo <jaborova.shahlo@mail.ru>
2020-10-03 01:56:48 +0500

    создана функция FindAccountByID и добавлены авто тесты

---
+++ pkg/wallet/service_test.go
@@ -0,0 +1,11 @@
+package wallet
+import "testing"
+func TestService_FindAccoundById_Method_NotFound(t *testing.T) {
+	svc := Service{}
+	svc.RegisterAccount("+9920000001")
+
+	account, err := svc.FindAccountByID(2)
+	if err == nil {
+		t.Errorf("\ngot > %v \nwant > nil", account)
+	}
+}
